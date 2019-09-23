8aa33633bdc5cfc18fdb34773cfa752226898263
Blessing Sanusi <sanusi.temitope@ymail.com>
2019-09-23 11:25:31 -0500

    adding working files

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "",
+    password: process.env.database_password,
     database: "burgers_db"
 });
 
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,20 +1,52 @@
 var express = require("express");
 
-var app = express();
+var router = express.Router();
 
-// Set the port of our application
-// process.env.PORT lets the port be set by Heroku
-var PORT = process.env.PORT || 8080;
+// Import the model (cat.js) to use its database functions.
+var burger = require("../models/burger");
 
-// Sets up the Express app to handle data parsing
-app.use(express.urlencoded({
-    extended: true
-}));
-app.use(express.json());
+// Create all our routes and set up logic within those routes where required.
+router.get("/", function (req, res) {
+    burger.all(function (data) {
+        var hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
 
-var exphbs = require("express-handlebars");
+router.post("/api/burgers", function (req, res) {
+    burger.create([
+        "burger_name"
+    ], [
+        req.body.burger_name
+    ], function (result) {
+        // Send back the ID of the new quote
+        res.json({
+            id: result.insertId
+        });
+    });
+});
 
-app.engine("handlebars", exphbs({
-    defaultLayout: "main"
-}));
-app.set("view engine", "handlebars");
+router.put("/api/burgers/:id", function (req, res) {
+    var condition = "id = " + req.params.id;
+
+    console.log("condition", condition);
+
+    burger.update({
+        devoured: req.body.devoured
+    }, condition, function (result) {
+        if (result.changedRows == 0) {
+            // If no rows were changed, then the ID must not exist, so 404
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+
+
+
+// Export routes for server.js to use.
+module.exports = router;
